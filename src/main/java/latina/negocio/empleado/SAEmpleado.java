4012293c6bbdabb6ac0adafc60abce7f1b47527e
amaiaech <amaiaech@ucm.es>
2025-03-22 12:47:43 +0100

    Función buscar empleados con disponibilidad

--- src/main/java/latina/negocio/empleado/SAEmpleado.java
+++ src/main/java/latina/negocio/empleado/SAEmpleado.java
@@ -1,4 +1,9 @@
 package latina.negocio.empleado;
 
+import latina.negocio.turno.Turno;
+
+import java.util.List;
+
 public interface SAEmpleado {
+    List<Empleado> getEmpleadosDisponibles(Turno turno);
 }
--- src/main/java/latina/negocio/empleado/imp/SAEmpleadoImp.java
+++ src/main/java/latina/negocio/empleado/imp/SAEmpleadoImp.java
@@ -1,4 +1,45 @@
 package latina.negocio.empleado.imp;
 
-public class SAEmpleadoImp {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import latina.integracion.emfc.EMFContainer;
+import latina.negocio.empleado.Empleado;
+import latina.negocio.empleado.SAEmpleado;
+import latina.negocio.turno.Turno;
+
+import java.util.List;
+
+public class SAEmpleadoImp implements SAEmpleado {
+    @Override
+    public List<Empleado> getEmpleadosDisponibles(Turno turno) {
+        EntityManager em = null;
+        List<Empleado> empleados = null;
+        try {
+            em = crearEntityManager();
+            //Obtiene los empleados de la tabla Disponibilidad que cubran la fecha y horas completas
+            TypedQuery<Empleado> query = em.createQuery(
+                    "SELECT DISTINCT d.empleado FROM Disponibilidad d " +
+                            "WHERE d.fechaInicio <= :fechaInicio AND d.fechaFin >= :fechaFin " +
+                            "AND d.horaInicio <= :horaInicio AND d.horaFin >= :horaFin",
+                    Empleado.class
+            );
+            //Convierte los valores de turno en strings, para que sean compatibles con la BD
+            query.setParameter("fechaInicio", turno.getFechaHoraInicio().toLocalDateTime().toLocalDate().toString());
+            query.setParameter("fechaFin", turno.getFechaHoraFin().toLocalDateTime().toLocalDate().toString());
+            query.setParameter("horaInicio", turno.getFechaHoraInicio().toLocalDateTime().toLocalTime().toString());
+            query.setParameter("horaFin", turno.getFechaHoraFin().toLocalDateTime().toLocalTime().toString());
+            empleados = query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return empleados;
+    }
+
+    protected EntityManager crearEntityManager() {
+        return EMFContainer.getInstance().getEMF().createEntityManager();
+    }
 }
