0510a4619ff7dc9848685f47b457209ff13dc568
amaury <amauryav@ucm.es>
2025-03-31 12:03:40 +0200

    arreglado error fatal cometido por mi

--- src/main/java/latina/negocio/empleado/Empleado.java
+++ src/main/java/latina/negocio/empleado/Empleado.java
@@ -126,29 +126,4 @@ public class Empleado {
         return new TEmpleado(id, DNI, nombre, apellidos, correo, telefono, activo);
     }
 
-
-
-    /// @return Una lista de empleados, una lista vacía si no existen, o null si se produce una excepción
-    public List<TEmpleado> buscarEmpleados(){
-        EntityTransaction tx = null;
-        try (EntityManager em = crearEntityManager()) {
-            tx = em.getTransaction();
-            tx.begin();
-            Query queryBuscarEmpleados = em.createNamedQuery("Empleado.findAll");
-            List<Empleado> empleados = (List<Empleado>) queryBuscarEmpleados.getResultList();
-            List<TEmpleado> resultado = new ArrayList<>();
-            for (Empleado e : empleados)
-                resultado.add(e.toTransfer());
-            return resultado;
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (tx != null && tx.isActive())
-                tx.rollback();
-            return null;
-        }
-    }
-
-    protected EntityManager crearEntityManager() {
-        return EMFContainer.getInstance().getEMF().createEntityManager();
-    }
 }
--- src/main/java/latina/negocio/empleado/imp/SAEmpleadoImp.java
+++ src/main/java/latina/negocio/empleado/imp/SAEmpleadoImp.java
@@ -1,9 +1,11 @@
 package latina.negocio.empleado.imp;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
 import latina.integracion.emfc.EMFContainer;
 import latina.negocio.disponibilidad.Disponibilidad;
+import latina.negocio.empleado.Empleado;
 import latina.negocio.empleado.SAEmpleado;
 import latina.negocio.empleado.TEmpleado;
 import latina.negocio.turno.Turno;
@@ -41,7 +43,25 @@ public class SAEmpleadoImp implements SAEmpleado {
     }
 
 
-
+    /// @return Una lista de empleados, una lista vacía si no existen, o null si se produce una excepción
+    public List<TEmpleado> buscarEmpleados(){
+        EntityTransaction tx = null;
+        try (EntityManager em = crearEntityManager()) {
+            tx = em.getTransaction();
+            tx.begin();
+            Query queryBuscarEmpleados = em.createNamedQuery("Empleado.findAll");
+            List<Empleado> empleados = (List<Empleado>) queryBuscarEmpleados.getResultList();
+            List<TEmpleado> resultado = new ArrayList<>();
+            for (Empleado e : empleados)
+                resultado.add(e.toTransfer());
+            return resultado;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tx != null && tx.isActive())
+                tx.rollback();
+            return null;
+        }
+    }
 
     protected EntityManager crearEntityManager() {
         return EMFContainer.getInstance().getEMF().createEntityManager();
