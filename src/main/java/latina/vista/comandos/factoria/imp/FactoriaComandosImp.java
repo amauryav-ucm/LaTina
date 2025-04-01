fd44f55efb4fd22c31d7f50fc89e18b9b6855578
amaury <amauryav@ucm.es>
2025-04-01 17:52:56 +0200

    cambiado paquete Turno a turno

--- src/main/java/latina/vista/comandos/factoria/imp/FactoriaComandosImp.java
+++ src/main/java/latina/vista/comandos/factoria/imp/FactoriaComandosImp.java
@@ -4,7 +4,6 @@ import latina.vista.Eventos;
 import latina.vista.comandos.Comando;
 import latina.vista.comandos.empleado.ObtenerEmpleadosDisponiblesInterfaz;
 import latina.vista.comandos.turno.AsignarTurnoEmpleado;
-import latina.vista.comandos.Turno.AsignarTurnoEmpleado;
 import latina.vista.comandos.Turno.VerTurnosSemanales;
 import latina.vista.comandos.factoria.FactoriaComandos;
 import latina.vista.comandos.rol.RegistrarRol;
